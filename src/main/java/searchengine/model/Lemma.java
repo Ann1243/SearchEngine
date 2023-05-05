fde92733e94f5675bac8b8aa8f278e659a34f6bf
Ann1243 <132685362+Ann1243@users.noreply.github.com>
2023-05-05 18:05:42 +0300

    Add files via upload

---
+++ src/main/java/searchengine/model/Lemma.java
@@ -0,0 +1,32 @@
+package searchengine.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "lemma")
+public class Lemma implements Comparable<Lemma>{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "lemma", nullable = false)
+    private String lemma;
+
+    @Column(name = "frequency", nullable = false)
+    private Integer frequency;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "site_id", referencedColumnName = "id")
+    private Site site;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "lemma")
+    private List<Index> indexes;
+    @Override
+    public int compareTo(Lemma o) {
+        return this.frequency.compareTo(o.frequency); }
+}
---
+++ src/main/java/searchengine/model/Page.java
@@ -0,0 +1,31 @@
+package searchengine.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+@Getter
+@Setter
+@Entity
+@Table(name = "page", indexes = {@Index(name = "path_index", columnList = "path")})
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "path", nullable = false, unique = true, columnDefinition = "VARCHAR(256)")
+    private String path;
+
+    @Column(name = "code", nullable = false)
+    private int code;
+
+    @Column(name = "content", nullable = false, columnDefinition = "MEDIUMTEXT")
+    private String content;
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "site_id", referencedColumnName = "id")
+    private Site site;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "page")
+    private List<Index> indexes ;
+}
